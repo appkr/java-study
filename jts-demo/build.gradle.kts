f46839b821d44ab52d84716af44cd91d47115034
appkr <juwonkim@me.com>
2019-08-05 16:30:44 +0900

    geoapi and geotools wip

--- jts-demo/build.gradle.kts
+++ jts-demo/build.gradle.kts
@@ -5,11 +5,15 @@ plugins {
 
 repositories {
     jcenter()
+    mavenCentral()
 }
 
 dependencies {
     implementation("com.google.guava:guava:27.0.1-jre")
     compile("org.locationtech.jts:jts-core:1.16.1")
+//    compile("org.opengis:geoapi:3.0.1")
+//    compile("org.geotools:gt-main:21.1")
+//    compile("javax.measure:unit-api:2.0")
     testImplementation("junit:junit:4.12")
 }
 
