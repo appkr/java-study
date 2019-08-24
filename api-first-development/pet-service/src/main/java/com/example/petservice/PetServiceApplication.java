01548866ef2bc6d85068c22cf435230e30509094
appkr <juwonkim@me.com>
2019-08-24 21:42:03 +0900

    pet-service API server project created

---
+++ api-first-development/pet-service/src/main/java/com/example/petservice/PetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.petservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetServiceApplication.class, args);
+    }
+
+}
