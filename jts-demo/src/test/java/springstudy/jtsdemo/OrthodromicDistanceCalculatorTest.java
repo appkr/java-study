c0689d143cfb5f28e9e1ba374638a30261bb9d65
appkr <juwonkim@me.com>
2019-08-29 20:57:52 +0900

    geoapi and geotools - Orthodromic geo-distance algorithm

--- jts-demo/src/test/java/springstudy/jtsdemo/OrthodromicDistanceCalculatorTest.java
+++ jts-demo/src/test/java/springstudy/jtsdemo/OrthodromicDistanceCalculatorTest.java
@@ -3,9 +3,9 @@ package springstudy.jtsdemo;
 import org.junit.Test;
 import org.locationtech.jts.geom.Coordinate;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-public class HaversineDistanceCalculatorTest {
+public class OrthodromicDistanceCalculatorTest {
 
     @Test
     public void testCalculate() {
@@ -14,13 +14,13 @@ public class HaversineDistanceCalculatorTest {
         // 동화면세점 @see https://geo.appkr.dev?lat=37.5697197&lng=126.9763056
         Coordinate sutB = new Coordinate(126.9763056, 37.5697197);
 
-        Distance distance = HaversineDistanceCalculator.calculate(sutA, sutB);
+        Distance distance = OrthodromicDistanceCalculator.calculate(sutA, sutB);
 
         System.out.println("---");
         System.out.println(String.format("Distance between %s and %s: %.1f%s",
             sutA, sutB, distance.getValue(), distance.getUnit().getAbbreviation()));
         System.out.println("---");
 
-        assertEquals(598.0, distance.roundUp(0).getValue(), 0.0);
+        assertEquals(597.0, distance.roundUp(0).getValue(), 0.0);
     }
 }
