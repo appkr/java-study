a51a3689609ea66a477992d6f0c710c9f537d53f
appkr <juwonkim@me.com>
2019-08-29 20:42:27 +0900

    geoapi and geotools - dependent library added

--- jts-demo/build.gradle.kts
+++ jts-demo/build.gradle.kts
@@ -4,16 +4,19 @@ plugins {
 }
 
 repositories {
-    jcenter()
+    maven {
+        setUrl("http://maven.geomajas.org")
+    }
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation("com.google.guava:guava:27.0.1-jre")
     compile("org.locationtech.jts:jts-core:1.16.1")
-//    compile("org.opengis:geoapi:3.0.1")
-//    compile("org.geotools:gt-main:21.1")
-//    compile("javax.measure:unit-api:2.0")
+    compile("org.locationtech.jts:jts-modules:1.16.1")
+    compile("org.geotools:gt-main:21.2")
+
     testImplementation("junit:junit:4.12")
 }
 
