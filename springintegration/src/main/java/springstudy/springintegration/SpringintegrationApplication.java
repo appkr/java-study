17efa24403ecae8e8726dfd9242fb7cbcfea2849
appkr <juwonkim@me.com>
2019-09-15 21:52:18 +0900

    New project created for SpringIntegration study

---
+++ springintegration/src/main/java/springstudy/springintegration/SpringintegrationApplication.java
@@ -0,0 +1,13 @@
+package springstudy.springintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringintegrationApplication.class, args);
+    }
+
+}
