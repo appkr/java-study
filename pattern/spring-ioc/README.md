f10b1140a9778e81408e6aa1e2404187f6f407ca
appkr <juwonkim@me.com>
2020-10-29 13:24:15 +0900

    spring ioc example - FactoryBean

--- pattern/spring-ioc/README.md
+++ pattern/spring-ioc/README.md
@@ -14,7 +14,8 @@ Tiger getTiger(String name) {
 Tiger tiger = context.getBean(Tiger.class, "Shere Khan");
 ```
 
-- module 3 @see https://www.baeldung.com/spring-factorybean
+- module 3 @see https://www.baeldung.com/spring-factorybean @see https://spring.io/blog/2011/08/09/what-s-a-factorybean
+> There are two kinds of beans in the Spring bean container: ordinary beans and factory beans. Spring uses the former directly, whereas latter can produce objects themselves, which are managed by the framework
 
 - module 4 @see https://www.baeldung.com/spring-beans-factory-methods
 
---
+++ pattern/spring-ioc/src/main/java/dev/appkr/springioc/module3/FactoryBeanConfig.java
@@ -0,0 +1,39 @@
+package dev.appkr.springioc.module3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FactoryBeanConfig {
+
+  @Bean(name = "tool")
+  public ToolFactory toolFactory() {
+    ToolFactory factory = new ToolFactory();
+    factory.setFactoryId(7070);
+    factory.setToolId(2);
+
+    return factory;
+  }
+
+  @Bean
+  public Tool tool() throws Exception {
+    return toolFactory().getObject();
+  }
+
+  @Bean
+  public CarFactory carFactory() {
+    CarFactory factory = new CarFactory();
+    factory.setMake("Ford");
+    factory.setYear(1984);
+
+    return factory;
+  }
+
+  @Bean
+  public Person person() throws Exception {
+    Person person = new Person();
+    person.setCar(carFactory().getObject());
+
+    return person;
+  }
+}
