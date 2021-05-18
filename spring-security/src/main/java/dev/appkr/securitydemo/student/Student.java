06c75df80bd2c9f5c5ba20cbad8bbfb9faf66cd7
appkr <juwonkim@me.com>
2021-05-18 14:43:53 +0900

    [spring-security] api

---
+++ spring-security/src/main/java/dev/appkr/securitydemo/student/Student.java
@@ -0,0 +1,27 @@
+package dev.appkr.securitydemo.student;
+
+public class Student {
+  private final Integer studentId;
+  private final String studentName;
+
+  public Student(Integer studentId, String studentName) {
+    this.studentId = studentId;
+    this.studentName = studentName;
+  }
+
+  public Integer getStudentId() {
+    return studentId;
+  }
+
+  public String getStudentName() {
+    return studentName;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+        "studentId=" + studentId +
+        ", studentName='" + studentName + '\'' +
+        '}';
+  }
+}
