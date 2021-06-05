75d49d8acbc635394f7e18a3fe74667084f37193
appkr <juwonkim@me.com>
2021-06-05 19:15:30 +0900

    [kotlin microservice] new springboot kotlin project

---
+++ kotlin-microservice/mvc/src/main/kotlin/dev/appkr/mvc/Application.kt
@@ -0,0 +1,11 @@
+package dev.appkr.mvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<MvcApplication>(*args)
+}
---
+++ kotlin-microservice/mvc/src/test/kotlin/dev/appkr/mvc/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.appkr.mvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
