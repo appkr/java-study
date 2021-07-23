6733f0dbc8007f726d13b453ed3947585b5612b7
appkr <juwonkim@me.com>
2021-07-23 17:25:00 +0900

    [social login] github login

--- social-login/src/main/java/dev/appkr/social/SocialApplication.java
+++ social-login/src/main/java/dev/appkr/social/SocialApplication.java
@@ -1,13 +1,45 @@
 package dev.appkr.social;
 
+import java.util.Collections;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SocialApplication {
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialApplication.class, args);
-	}
+  @GetMapping("/user")
+  public OAuth2User user(@AuthenticationPrincipal OAuth2User principal) {
+    return principal;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // @formatter:off
+    http
+        .authorizeRequests(a -> a
+            .antMatchers("/", "/error").permitAll()
+            .anyRequest().authenticated()
+        )
+        .exceptionHandling(e -> e
+            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        )
+        .oauth2Login();
+    // @formatter:on
+  }
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(SocialApplication.class, args);
+  }
 
 }
