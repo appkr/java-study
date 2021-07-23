ae5c74b8e85dffede69ebdc1921af8eb89ff69ea
appkr <juwonkim@me.com>
2021-07-23 16:55:57 +0900

    [social login] new project created

---
+++ social-login/src/test/java/dev/appkr/social/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.appkr.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
