600b93eb157034c2567fbfd601b1bac5907d14e5
appkr <juwonkim@me.com>
2022-04-03 14:38:18 +0900

    [performance-webmvc] new project

---
+++ performance-webmvc/src/main/java/dev/appkr/webmvc/Webmvc.java
@@ -0,0 +1,13 @@
+package dev.appkr.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webmvc {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Webmvc.class, args);
+  }
+
+}
