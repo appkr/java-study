3ce01a1755f189f14a08c4ea51ae531a66326984
appkr <juwonkim@me.com>
2022-11-16 16:55:09 +0900

    TcpServer 기본 구현

--- java-socket4/src/main/java/dev/appkr/demo/Application.java
+++ java-socket4/src/main/java/dev/appkr/demo/Application.java
@@ -1,13 +1,29 @@
 package dev.appkr.demo;
 
+import dev.appkr.demo.tcp.server.TcpServer;
+import dev.appkr.demo.tcp.config.TcpServerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TcpServerProperties.class)
 public class Application {
+
+	final TcpServer tcpServer;
+
+	public Application(TcpServer tcpServer) {
+		this.tcpServer = tcpServer;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@EventListener
+	void onApplicationReadyEvent(ApplicationReadyEvent e) {
+		tcpServer.start();
+	}
 }
