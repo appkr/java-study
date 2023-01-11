8e7dce6f4a513d061668d101c66c3e55c29c6882
appkr <juwonkim@me.com>
2023-01-11 18:02:44 +0900

    [java-socket5] new project

---
+++ java-socket5/src/main/java/dev/appkr/demo/App.java
@@ -0,0 +1,8 @@
+package dev.appkr.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ java-socket5/src/test/java/dev/appkr/demo/AppTest.java
@@ -0,0 +1,9 @@
+package dev.appkr.demo;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void appHasAGreeting() {
+    }
+}
