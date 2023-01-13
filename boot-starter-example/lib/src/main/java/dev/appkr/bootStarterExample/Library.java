95ffda045a857b7cf1fa9e7eed077b38e684d208
appkr <juwonkim@me.com>
2023-01-13 17:33:56 +0900

    [spring-boot-starter-example] new project

---
+++ boot-starter-example/lib/src/main/java/dev/appkr/bootStarterExample/Library.java
@@ -0,0 +1,7 @@
+package dev.appkr.bootStarterExample;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ boot-starter-example/lib/src/test/java/dev/appkr/bootStarterExample/LibraryTest.java
@@ -0,0 +1,11 @@
+package dev.appkr.bootStarterExample;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+    @Test void someLibraryMethodReturnsTrue() {
+        Library classUnderTest = new Library();
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    }
+}
