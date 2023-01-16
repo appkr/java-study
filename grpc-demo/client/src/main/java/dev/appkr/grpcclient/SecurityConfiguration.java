bf7f901be5f9a488b04892b8b39bdab8e83fe993
appkr <juwonkim@me.com>
2023-01-16 11:07:48 +0900

    [grpc-demo] library version up

--- grpc-demo/client/src/main/java/dev/appkr/grpcclient/SecurityConfiguration.java
+++ grpc-demo/client/src/main/java/dev/appkr/grpcclient/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package dev.appkr.grpcclient;
 
+import static org.springframework.security.oauth2.jwt.NimbusJwtDecoder.withJwkSetUri;
+
 import io.grpc.CallCredentials;
-import io.grpc.Metadata;
+import net.devh.boot.grpc.client.security.CallCredentialsHelper;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +18,6 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.concurrent.Executor;
-import java.util.function.Supplier;
-
-import static net.devh.boot.grpc.common.security.SecurityConstants.AUTHORIZATION_HEADER;
-import static net.devh.boot.grpc.common.security.SecurityConstants.BEARER_AUTH_PREFIX;
-import static org.springframework.security.oauth2.jwt.NimbusJwtDecoder.withJwkSetUri;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
@@ -36,46 +31,45 @@ public class SecurityConfiguration {
 
   @Bean
   CallCredentials bearerAuthForwardingCredentials() {
-// NOTE. the following code block is not working...
-// Which is a bug of 2.13.1 @see https://github.com/yidongnan/grpc-spring-boot-starter/pull/642
+    return CallCredentialsHelper.bearerAuth(() -> {
+      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication != null && authentication instanceof JwtAuthenticationToken) {
+        final Jwt jwt = (Jwt) authentication.getPrincipal();
+        return jwt.getTokenValue();
+      }
 
-//    return CallCredentialsHelper.bearerAuth(() -> {
-//      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//      if (authentication != null && authentication instanceof OAuth2Authentication) {
-//        final OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
-//        return details.getTokenValue();
+      return null;
+    });
+
+// NOTE. the following code was used to workaround a bug of 2.13.1 @see https://github.com/yidongnan/grpc-spring-boot-starter/pull/642
+//       the following code block is no more required as of 2.14.0
+//    return new CallCredentials() {
+//
+//      private final Supplier<Metadata> extraHeadersSupplier = () -> {
+//        final Metadata extraHeaders = new Metadata();
+//
+//        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (authentication != null && authentication instanceof JwtAuthenticationToken) {
+//          final Jwt jwt = (Jwt) authentication.getPrincipal();
+//          extraHeaders.put(AUTHORIZATION_HEADER, BEARER_AUTH_PREFIX + jwt.getTokenValue());
+//        }
+//
+//        return extraHeaders;
+//      };
+//
+//      @Override
+//      public void applyRequestMetadata(final RequestInfo requestInfo, final Executor appExecutor, final MetadataApplier applier) {
+//        applier.apply(this.extraHeadersSupplier.get());
 //      }
 //
-//      return null;
-//    });
-
-    return new CallCredentials() {
-
-      private final Supplier<Metadata> extraHeadersSupplier = () -> {
-        final Metadata extraHeaders = new Metadata();
-
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication instanceof JwtAuthenticationToken) {
-          final Jwt jwt = (Jwt) authentication.getPrincipal();
-          extraHeaders.put(AUTHORIZATION_HEADER, BEARER_AUTH_PREFIX + jwt.getTokenValue());
-        }
-
-        return extraHeaders;
-      };
-
-      @Override
-      public void applyRequestMetadata(final RequestInfo requestInfo, final Executor appExecutor, final MetadataApplier applier) {
-        applier.apply(this.extraHeadersSupplier.get());
-      }
-
-      @Override
-      public void thisUsesUnstableApi() {} // API evolution in progress
-
-      @Override
-      public String toString() {
-        return "CallCredentials [extraHeadersSupplier=" + this.extraHeadersSupplier + "]";
-      }
-    };
+//      @Override
+//      public void thisUsesUnstableApi() {} // API evolution in progress
+//
+//      @Override
+//      public String toString() {
+//        return "CallCredentials [extraHeadersSupplier=" + this.extraHeadersSupplier + "]";
+//      }
+//    };
   }
 
   @Bean
--- grpc-demo/proto/src/generated/main/java/dev/appkr/grpcdemo/HelloReply.java
+++ grpc-demo/proto/src/generated/main/java/dev/appkr/grpcdemo/HelloReply.java
@@ -32,60 +32,6 @@ private static final long serialVersionUID = 0L;
   getUnknownFields() {
     return this.unknownFields;
   }
-  private HelloReply(
-      com.google.protobuf.CodedInputStream input,
-      com.google.protobuf.ExtensionRegistryLite extensionRegistry)
-      throws com.google.protobuf.InvalidProtocolBufferException {
-    this();
-    if (extensionRegistry == null) {
-      throw new java.lang.NullPointerException();
-    }
-    com.google.protobuf.UnknownFieldSet.Builder unknownFields =
-        com.google.protobuf.UnknownFieldSet.newBuilder();
-    try {
-      boolean done = false;
-      while (!done) {
-        int tag = input.readTag();
-        switch (tag) {
-          case 0:
-            done = true;
-            break;
-          case 10: {
-            java.lang.String s = input.readStringRequireUtf8();
-
-            message_ = s;
-            break;
-          }
-          case 16: {
-
-            luckyNumber_ = input.readInt32();
-            break;
-          }
-          case 26: {
-            java.lang.String s = input.readStringRequireUtf8();
-
-            principal_ = s;
-            break;
-          }
-          default: {
-            if (!parseUnknownField(
-                input, unknownFields, extensionRegistry, tag)) {
-              done = true;
-            }
-            break;
-          }
-        }
-      }
-    } catch (com.google.protobuf.InvalidProtocolBufferException e) {
-      throw e.setUnfinishedMessage(this);
-    } catch (java.io.IOException e) {
-      throw new com.google.protobuf.InvalidProtocolBufferException(
-          e).setUnfinishedMessage(this);
-    } finally {
-      this.unknownFields = unknownFields.build();
-      makeExtensionsImmutable();
-    }
-  }
   public static final com.google.protobuf.Descriptors.Descriptor
       getDescriptor() {
     return dev.appkr.grpcdemo.Hello.internal_static_grpcdemo_HelloReply_descriptor;
@@ -100,7 +46,8 @@ private static final long serialVersionUID = 0L;
   }
 
   public static final int MESSAGE_FIELD_NUMBER = 1;
-  private volatile java.lang.Object message_;
+  @SuppressWarnings("serial")
+  private volatile java.lang.Object message_ = "";
   /**
    * <code>string message = 1;</code>
    * @return The message.
@@ -138,7 +85,7 @@ private static final long serialVersionUID = 0L;
   }
 
   public static final int LUCKYNUMBER_FIELD_NUMBER = 2;
-  private int luckyNumber_;
+  private int luckyNumber_ = 0;
   /**
    * <code>int32 luckyNumber = 2;</code>
    * @return The luckyNumber.
@@ -149,7 +96,8 @@ private static final long serialVersionUID = 0L;
   }
 
   public static final int PRINCIPAL_FIELD_NUMBER = 3;
-  private volatile java.lang.Object principal_;
+  @SuppressWarnings("serial")
+  private volatile java.lang.Object principal_ = "";
   /**
    * <code>string principal = 3;</code>
    * @return The principal.
@@ -209,7 +157,7 @@ private static final long serialVersionUID = 0L;
     if (!com.google.protobuf.GeneratedMessageV3.isStringEmpty(principal_)) {
       com.google.protobuf.GeneratedMessageV3.writeString(output, 3, principal_);
     }
-    unknownFields.writeTo(output);
+    getUnknownFields().writeTo(output);
   }
 
   @java.lang.Override
@@ -228,7 +176,7 @@ private static final long serialVersionUID = 0L;
     if (!com.google.protobuf.GeneratedMessageV3.isStringEmpty(principal_)) {
       size += com.google.protobuf.GeneratedMessageV3.computeStringSize(3, principal_);
     }
-    size += unknownFields.getSerializedSize();
+    size += getUnknownFields().getSerializedSize();
     memoizedSize = size;
     return size;
   }
@@ -249,7 +197,7 @@ private static final long serialVersionUID = 0L;
         != other.getLuckyNumber()) return false;
     if (!getPrincipal()
         .equals(other.getPrincipal())) return false;
-    if (!unknownFields.equals(other.unknownFields)) return false;
+    if (!getUnknownFields().equals(other.getUnknownFields())) return false;
     return true;
   }
 
@@ -266,7 +214,7 @@ private static final long serialVersionUID = 0L;
     hash = (53 * hash) + getLuckyNumber();
     hash = (37 * hash) + PRINCIPAL_FIELD_NUMBER;
     hash = (53 * hash) + getPrincipal().hashCode();
-    hash = (29 * hash) + unknownFields.hashCode();
+    hash = (29 * hash) + getUnknownFields().hashCode();
     memoizedHashCode = hash;
     return hash;
   }
@@ -383,28 +331,21 @@ private static final long serialVersionUID = 0L;
 
     // Construct using dev.appkr.grpcdemo.HelloReply.newBuilder()
     private Builder() {
-      maybeForceBuilderInitialization();
+
     }
 
     private Builder(
         com.google.protobuf.GeneratedMessageV3.BuilderParent parent) {
       super(parent);
-      maybeForceBuilderInitialization();
-    }
-    private void maybeForceBuilderInitialization() {
-      if (com.google.protobuf.GeneratedMessageV3
-              .alwaysUseFieldBuilders) {
-      }
+
     }
     @java.lang.Override
     public Builder clear() {
       super.clear();
+      bitField0_ = 0;
       message_ = "";
-
       luckyNumber_ = 0;
-
       principal_ = "";
-
       return this;
     }
 
@@ -431,11 +372,22 @@ private static final long serialVersionUID = 0L;
     @java.lang.Override
     public dev.appkr.grpcdemo.HelloReply buildPartial() {
       dev.appkr.grpcdemo.HelloReply result = new dev.appkr.grpcdemo.HelloReply(this);
-      result.message_ = message_;
-      result.luckyNumber_ = luckyNumber_;
-      result.principal_ = principal_;
+      if (bitField0_ != 0) { buildPartial0(result); }
       onBuilt();
       return result;
+    }
+
+    private void buildPartial0(dev.appkr.grpcdemo.HelloReply result) {
+      int from_bitField0_ = bitField0_;
+      if (((from_bitField0_ & 0x00000001) != 0)) {
+        result.message_ = message_;
+      }
+      if (((from_bitField0_ & 0x00000002) != 0)) {
+        result.luckyNumber_ = luckyNumber_;
+      }
+      if (((from_bitField0_ & 0x00000004) != 0)) {
+        result.principal_ = principal_;
+      }
     }
 
     @java.lang.Override
@@ -484,6 +436,7 @@ private static final long serialVersionUID = 0L;
       if (other == dev.appkr.grpcdemo.HelloReply.getDefaultInstance()) return this;
       if (!other.getMessage().isEmpty()) {
         message_ = other.message_;
+        bitField0_ |= 0x00000001;
         onChanged();
       }
       if (other.getLuckyNumber() != 0) {
@@ -491,9 +444,10 @@ private static final long serialVersionUID = 0L;
       }
       if (!other.getPrincipal().isEmpty()) {
         principal_ = other.principal_;
+        bitField0_ |= 0x00000004;
         onChanged();
       }
-      this.mergeUnknownFields(other.unknownFields);
+      this.mergeUnknownFields(other.getUnknownFields());
       onChanged();
       return this;
     }
@@ -508,19 +462,48 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws java.io.IOException {
-      dev.appkr.grpcdemo.HelloReply parsedMessage = null;
+      if (extensionRegistry == null) {
+        throw new java.lang.NullPointerException();
+      }
       try {
-        parsedMessage = PARSER.parsePartialFrom(input, extensionRegistry);
+        boolean done = false;
+        while (!done) {
+          int tag = input.readTag();
+          switch (tag) {
+            case 0:
+              done = true;
+              break;
+            case 10: {
+              message_ = input.readStringRequireUtf8();
+              bitField0_ |= 0x00000001;
+              break;
+            } // case 10
+            case 16: {
+              luckyNumber_ = input.readInt32();
+              bitField0_ |= 0x00000002;
+              break;
+            } // case 16
+            case 26: {
+              principal_ = input.readStringRequireUtf8();
+              bitField0_ |= 0x00000004;
+              break;
+            } // case 26
+            default: {
+              if (!super.parseUnknownField(input, extensionRegistry, tag)) {
+                done = true; // was an endgroup tag
+              }
+              break;
+            } // default:
+          } // switch (tag)
+        } // while (!done)
       } catch (com.google.protobuf.InvalidProtocolBufferException e) {
-        parsedMessage = (dev.appkr.grpcdemo.HelloReply) e.getUnfinishedMessage();
         throw e.unwrapIOException();
       } finally {
-        if (parsedMessage != null) {
-          mergeFrom(parsedMessage);
-        }
-      }
+        onChanged();
+      } // finally
       return this;
     }
+    private int bitField0_;
 
     private java.lang.Object message_ = "";
     /**
@@ -563,11 +546,9 @@ private static final long serialVersionUID = 0L;
      */
     public Builder setMessage(
         java.lang.String value) {
-      if (value == null) {
-    throw new NullPointerException();
-  }
-  
+      if (value == null) { throw new NullPointerException(); }
       message_ = value;
+      bitField0_ |= 0x00000001;
       onChanged();
       return this;
     }
@@ -576,8 +557,8 @@ private static final long serialVersionUID = 0L;
      * @return This builder for chaining.
      */
     public Builder clearMessage() {
-      
       message_ = getDefaultInstance().getMessage();
+      bitField0_ = (bitField0_ & ~0x00000001);
       onChanged();
       return this;
     }
@@ -588,12 +569,10 @@ private static final long serialVersionUID = 0L;
      */
     public Builder setMessageBytes(
         com.google.protobuf.ByteString value) {
-      if (value == null) {
-    throw new NullPointerException();
-  }
-  checkByteStringIsUtf8(value);
-      
+      if (value == null) { throw new NullPointerException(); }
+      checkByteStringIsUtf8(value);
       message_ = value;
+      bitField0_ |= 0x00000001;
       onChanged();
       return this;
     }
@@ -615,6 +594,7 @@ private static final long serialVersionUID = 0L;
     public Builder setLuckyNumber(int value) {
       
       luckyNumber_ = value;
+      bitField0_ |= 0x00000002;
       onChanged();
       return this;
     }
@@ -623,7 +603,7 @@ private static final long serialVersionUID = 0L;
      * @return This builder for chaining.
      */
     public Builder clearLuckyNumber() {
-      
+      bitField0_ = (bitField0_ & ~0x00000002);
       luckyNumber_ = 0;
       onChanged();
       return this;
@@ -670,11 +650,9 @@ private static final long serialVersionUID = 0L;
      */
     public Builder setPrincipal(
         java.lang.String value) {
-      if (value == null) {
-    throw new NullPointerException();
-  }
-  
+      if (value == null) { throw new NullPointerException(); }
       principal_ = value;
+      bitField0_ |= 0x00000004;
       onChanged();
       return this;
     }
@@ -683,8 +661,8 @@ private static final long serialVersionUID = 0L;
      * @return This builder for chaining.
      */
     public Builder clearPrincipal() {
-      
       principal_ = getDefaultInstance().getPrincipal();
+      bitField0_ = (bitField0_ & ~0x00000004);
       onChanged();
       return this;
     }
@@ -695,12 +673,10 @@ private static final long serialVersionUID = 0L;
      */
     public Builder setPrincipalBytes(
         com.google.protobuf.ByteString value) {
-      if (value == null) {
-    throw new NullPointerException();
-  }
-  checkByteStringIsUtf8(value);
-      
+      if (value == null) { throw new NullPointerException(); }
+      checkByteStringIsUtf8(value);
       principal_ = value;
+      bitField0_ |= 0x00000004;
       onChanged();
       return this;
     }
@@ -737,7 +713,18 @@ private static final long serialVersionUID = 0L;
         com.google.protobuf.CodedInputStream input,
         com.google.protobuf.ExtensionRegistryLite extensionRegistry)
         throws com.google.protobuf.InvalidProtocolBufferException {
-      return new HelloReply(input, extensionRegistry);
+      Builder builder = newBuilder();
+      try {
+        builder.mergeFrom(input, extensionRegistry);
+      } catch (com.google.protobuf.InvalidProtocolBufferException e) {
+        throw e.setUnfinishedMessage(builder.buildPartial());
+      } catch (com.google.protobuf.UninitializedMessageException e) {
+        throw e.asInvalidProtocolBufferException().setUnfinishedMessage(builder.buildPartial());
+      } catch (java.io.IOException e) {
+        throw new com.google.protobuf.InvalidProtocolBufferException(e)
+            .setUnfinishedMessage(builder.buildPartial());
+      }
+      return builder.buildPartial();
     }
   };
 
