3cb20b0c176eb1faf58337f3edb576a73fb8ebee
appkr <juwonkim@me.com>
2023-03-04 09:48:36 +0900

    [gradle-multi-module-example] new project

---
+++ gradle-multi-module-project-example/src/main/java/dev/appkr/example/GradleMultiModuleProjectExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.appkr.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleMultiModuleProjectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleMultiModuleProjectExampleApplication.class, args);
+	}
+
+}
---
+++ gradle-multi-module-project-example/src/test/java/dev/appkr/example/GradleMultiModuleProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.appkr.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleMultiModuleProjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
