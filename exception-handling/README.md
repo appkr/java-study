11d3bb00703c2768ae9f9bebc404372e20988b64
appkr <juwonkim@me.com>
2023-05-24 20:28:40 +0900

    [exception-handling] for Stream

---
+++ exception-handling/README.md
@@ -0,0 +1,13 @@
+## Exception Handling Exercise
+
+### Optional
+
+### Stream
+- https://dzone.com/articles/exception-handling-in-java-streams
+- CheckedFunction/Either/Pair
+
+### CompletableFuture
+
+### Mono
+
+### Flux
---
+++ exception-handling/src/main/java/dev/appkr/demo/stream/CheckedFunction.java
@@ -0,0 +1,19 @@
+package dev.appkr.demo.stream;
+
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface CheckedFunction<T,R> {
+
+  R apply(T t) throws Exception;
+
+  static <T,R> Function<T,R> wrap(CheckedFunction<T,R> checkedFunction) {
+    return t -> {
+      try {
+        return checkedFunction.apply(t);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    };
+  }
+}
