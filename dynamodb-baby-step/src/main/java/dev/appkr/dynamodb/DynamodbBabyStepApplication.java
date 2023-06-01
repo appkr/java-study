5112b9fd73bd6338749a145d8d808dcd39461ee9
appkr <juwonkim@me.com>
2023-06-01 11:39:41 +0900

    [dynamodb-baby-step] new project

---
+++ dynamodb-baby-step/src/main/java/dev/appkr/dynamodb/DynamodbBabyStepApplication.java
@@ -0,0 +1,13 @@
+package dev.appkr.dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbBabyStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbBabyStepApplication.class, args);
+	}
+
+}
---
+++ dynamodb-baby-step/src/test/java/dev/appkr/dynamodb/DynamodbBabyStepApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.appkr.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbBabyStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
