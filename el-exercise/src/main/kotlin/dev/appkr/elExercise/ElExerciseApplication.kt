ffcb2cd14c8476b12528807a58ebd3444cff2c06
appkr <juwonkim@me.com>
2024-06-29 18:01:55 +0900

    [el-exercise] new project

---
+++ el-exercise/src/main/kotlin/dev/appkr/elExercise/ElExerciseApplication.kt
@@ -0,0 +1,11 @@
+package dev.appkr.elExercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ElExerciseApplication>(*args)
+}
---
+++ el-exercise/src/test/kotlin/dev/appkr/elExercise/ElExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.appkr.elExercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
