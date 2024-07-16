fbdfb2ed09b06dcb9271b77a318d8f6c81b5423f
appkr <juwonkim@me.com>
2024-07-16 15:37:09 +0900

    [adjacency-list] new project

---
+++ adjacency-list/src/main/kotlin/dev/appkr/adjacencyList/AdjacencyListApplication.kt
@@ -0,0 +1,11 @@
+package dev.appkr.adjacencyList
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdjacencyListApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdjacencyListApplication>(*args)
+}
---
+++ adjacency-list/src/test/kotlin/dev/appkr/adjacencyList/AdjacencyListApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.appkr.adjacencyList
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdjacencyListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
